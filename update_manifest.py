63fc1b4a3021827fc1deae2ec314dd57b93fb45d
Roman Imankulov <roman.imankulov@gmail.com>
2023-08-11 14:46:41 +0200

    Add meta.stackoverflow.com to the manifest

--- update_manifest.py
+++ update_manifest.py
@@ -4,12 +4,18 @@ import json
 import xml.etree.ElementTree as ET
 
 
+HARDCODED_SITES = [
+    "https://meta.stackoverflow.com",
+]
+
+
 
 def get_stackexchange_sites():
     feed = requests.get('https://stackexchange.com/feeds/sites').text
     root = ET.fromstring(feed)
     namespace = {'atom': 'http://www.w3.org/2005/Atom'}
     entry_ids = [entry.find('atom:id', namespace).text for entry in root.findall('atom:entry', namespace)]
+    entry_ids.extend(HARDCODED_SITES)
     return [f"{site}/*" for site in sorted(entry_ids)]
 
 def overwrite_manifest_json(sites: list[str]):
