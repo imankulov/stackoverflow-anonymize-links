e614bfc704cdbca312da8bb0f0a86e52338221bc
Roman Imankulov <roman.imankulov@gmail.com>
2023-08-11 14:46:18 +0200

    Fix the content selector

--- scripts/content.js
+++ scripts/content.js
@@ -1,7 +1,14 @@
 /*
  * Remove the user id from the end of the URL.
  */
-document.querySelectorAll("a[data-controller='se-share-sheet']").forEach(function(element) {
-    element.href = element.href.replace(/\d+$/, "").replace(/\/$/, "");
-    element.setAttribute("data-se-share-sheet-subtitle", "(Without your user id)");
+document.querySelectorAll("a[data-controller*='se-share-sheet']").forEach(function(element) {
+    // First, see if the link matches the pattern q/12345/12345 or a/12345/12345.
+    // If not then we don't need to do anything.
+    if (!element.href.match(/\/(q|a)\/\d+\/\d+\/?$/)) {
+        return;
+    }
+    // Delete user ID from the link
+    element.href = element.href.replace(/\/\d+$/, "")
+    // Delete a warning about the user ID
+    element.removeAttribute("data-se-share-sheet-subtitle");
 });
